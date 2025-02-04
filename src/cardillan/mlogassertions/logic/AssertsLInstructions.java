c543af0e5ac119689d187a76f0f0aff23f8aa3b3
cardillan <122014763+cardillan@users.noreply.github.com>
2025-02-04 12:43:34 +0100

    Logic 7 version

--- src/cardillan/mlogassertions/logic/AssertsLInstructions.java
+++ src/cardillan/mlogassertions/logic/AssertsLInstructions.java
@@ -1,20 +1,20 @@
 package cardillan.mlogassertions.logic;
 
 import mindustry.logic.LExecutor;
-import mindustry.logic.LVar;
+import mindustry.logic.LExecutor.Var;
 
 public class AssertsLInstructions {
 
     public static class AssertI implements LExecutor.LInstruction {
-        public TypeAssertion type;
-        public LVar min;
-        public AssertOp opMin;
-        public LVar value;
-        public AssertOp opMax;
-        public LVar max;
-        public LVar message;
+        public TypeAssertion type = TypeAssertion.any;
+        public int min;
+        public AssertOp opMin = AssertOp.lessThanEq;
+        public int value;
+        public AssertOp opMax = AssertOp.lessThanEq;
+        public int max;
+        public int message;
 
-        public AssertI(TypeAssertion type, LVar min, AssertOp opMin, LVar value, AssertOp opMax, LVar max, LVar message) {
+        public AssertI(TypeAssertion type, int min, AssertOp opMin, int value, AssertOp opMax, int max, int message) {
             this.type = type;
             this.min = min;
             this.opMin = opMin;
@@ -24,18 +24,32 @@ public class AssertsLInstructions {
             this.message = message;
         }
 
+        public AssertI() {
+        }
+
         @Override
         public final void run(LExecutor exec) {
-            boolean typeAssert = value.isobj ? type.objFunction.get(value.objval) : type.function.get(value.numval);
-            boolean minAssert = opMin.function.get(min.num(), value.num());
-            boolean maxAssert = opMax.function.get(value.num(), max.num());
+            Var vaVal = var(exec, value);
+            boolean typeAssert = vaVal.isobj ? type.objFunction.get(vaVal.objval) : type.function.get(vaVal.numval);
+            boolean minAssert = opMin.function.get(num(exec,min), num(exec,value));
+            boolean maxAssert = opMax.function.get(num(exec,value), num(exec,max));
 
             if (!typeAssert || !minAssert || !maxAssert) {
                 //skip back to self.
                 // TODO enter broken assertion state
                 exec.counter.numval--;
-                exec.yield = true;
+                //exec.yield = true;
             }
+        }
+
+        // For easier switching between v7 and v8 logic
+
+        private Var var(LExecutor exec, int var) {
+            return exec.var(value);
+        }
+
+        private double num(LExecutor exec, int var) {
+            return exec.num(var);
         }
     }
 }
